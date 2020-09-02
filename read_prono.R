321518ef87b6750db6810b066b045f735441a9de
paocorrales <paobcorrales@gmail.com>
2020-09-02 00:45:40 -0300

    Build de prueba. Solo pronóstico.

--- read_prono.R
+++ read_prono.R
@@ -1,5 +1,5 @@
 # Lectura de la planilla desde drive
-
+library(tidyverse)
 library(googlesheets4)
 
 loc1 <- read_sheet("https://docs.google.com/spreadsheets/d/1c5Ms_V3OUdvj8RSULipN5ZmKYsgtd9tDYJxfMy_JHVo/edit#",
@@ -13,3 +13,9 @@ loc2 <- read_sheet("https://docs.google.com/spreadsheets/d/1c5Ms_V3OUdvj8RSULipN
 loc3 <- read_sheet("https://docs.google.com/spreadsheets/d/1c5Ms_V3OUdvj8RSULipN5ZmKYsgtd9tDYJxfMy_JHVo/edit#",
                    sheet = "Localidad 3",
                    skip = 3)
+
+test <- read_sheet("https://docs.google.com/spreadsheets/d/1CZ608ea9joU7aEeuSztD-Yqm_GAoxecUNt2bjMtLnWk/edit#",
+                   sheet = "Localidad 1",
+                   skip = 3) %>% 
+  janitor::clean_names() %>% 
+  mutate(ciudad = "test")
