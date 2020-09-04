83dd9d7746c9db68851e0ba911340c932e0d5a30
paocorrales <paobcorrales@gmail.com>
2020-09-03 21:15:48 -0300

    Mi primer pronóstico!

--- read_prono.R
+++ read_prono.R
@@ -3,16 +3,22 @@ library(tidyverse)
 library(googlesheets4)
 
 loc1 <- read_sheet("https://docs.google.com/spreadsheets/d/1c5Ms_V3OUdvj8RSULipN5ZmKYsgtd9tDYJxfMy_JHVo/edit#",
-           sheet = "Localidad 1",
-           skip = 3)
+           sheet = "CABA",
+           skip = 3) %>% 
+  janitor::clean_names() %>% 
+  mutate(ciudad = "Ciudad de Buenos Aires")
 
 loc2 <- read_sheet("https://docs.google.com/spreadsheets/d/1c5Ms_V3OUdvj8RSULipN5ZmKYsgtd9tDYJxfMy_JHVo/edit#",
-                   sheet = "Localidad 2",
-                   skip = 3)
+                   sheet = "Salta",
+                   skip = 3) %>% 
+  janitor::clean_names() %>% 
+  mutate(ciudad = "Salta")
 
 loc3 <- read_sheet("https://docs.google.com/spreadsheets/d/1c5Ms_V3OUdvj8RSULipN5ZmKYsgtd9tDYJxfMy_JHVo/edit#",
-                   sheet = "Localidad 3",
-                   skip = 3)
+                   sheet = "Cordoba",
+                   skip = 3) %>% 
+  janitor::clean_names() %>% 
+  mutate(ciudad = "Córdoba Aero")
 
 test <- read_sheet("https://docs.google.com/spreadsheets/d/1CZ608ea9joU7aEeuSztD-Yqm_GAoxecUNt2bjMtLnWk/edit#",
                    sheet = "Localidad 1",
